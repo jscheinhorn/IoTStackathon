6b9adfb1f5a70d765f3f8d6767334c95f1d6b272
Jennifer Scheinhorn <jscheinhorn@gmail.com>
2019-06-22 00:52:22 -0400

    feat: can access esp8266 data via redux store

--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
-import {me} from './store'
+import {me, getDataThunk} from './store'
 
 /**
  * COMPONENT
@@ -11,6 +11,7 @@ import {me} from './store'
 class Routes extends Component {
   componentDidMount() {
     this.props.loadInitialData()
+    this.props.dataThunk()
   }
 
   render() {
@@ -37,21 +38,17 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
-  }
-}
+const mapState = state => ({
+  // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+  // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+  isLoggedIn: !!state.user.id,
+  data: state.data
+})
 
-const mapDispatch = dispatch => {
-  return {
-    loadInitialData() {
-      dispatch(me())
-    }
-  }
-}
+const mapDispatch = dispatch => ({
+  loadInitialData: () => dispatch(me()),
+  dataThunk: () => dispatch(getDataThunk())
+})
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
---
+++ client/store/data.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_DATA = 'GET_DATA'
+
+/**
+ * INITIAL STATE
+ */
+const defaultData = {'x-axis': 0, 'y-axis': 0, 'z-axis': 0}
+
+/**
+ * ACTION CREATORS
+ */
+const getData = data => ({type: GET_DATA, data})
+
+/**
+ * THUNK CREATORS
+ */
+export const getDataThunk = () => async dispatch => {
+  try {
+    const data = await fetch('http://esp8266.local')
+    dispatch(getData((await data.json()) || defaultData))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultData, action) {
+  switch (action.type) {
+    case GET_DATA:
+      return {
+        ...state,
+        'x-axis': action.data.x,
+        'y-axis': action.data.y,
+        'z-axis': action.data.z
+      }
+    default:
+      return state
+  }
+}
